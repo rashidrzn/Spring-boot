0b3884f26832b3aa8e4ad47bb8bfa475836c5885
Rashid Risan <95292841+rashidrzn@users.noreply.github.com>
2023-05-22 13:30:02 +0530

    saveUser method created

--- src/main/java/com/example/simpleroot/SimplerootApplication.java
+++ src/main/java/com/example/simpleroot/SimplerootApplication.java
@@ -1,7 +1,9 @@
 package com.example.simpleroot;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimplerootApplication {
@@ -9,5 +11,9 @@ public class SimplerootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SimplerootApplication.class, args);
     }
+        @Bean
+        public ModelMapper modelMapper(){
+            return new ModelMapper();
+        }
+    }
 
-}
--- src/main/java/com/example/simpleroot/controller/UserController.java
+++ src/main/java/com/example/simpleroot/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.simpleroot.controller;
 
+import com.example.simpleroot.dto.UserDTO;
+import com.example.simpleroot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,14 +10,17 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/getUser")
     public String getUser() {
         return "Simple-Root";
     }
 
     @PostMapping("/saveUser")
-    public String saveUser() {
-        return "User Saved!";
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+        return userService.saveUser(userDTO);
     }
 
     @PutMapping("/updateUser")
